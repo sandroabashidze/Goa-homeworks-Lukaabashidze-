73fadd43b53878384631723b3623b576c3f830a6
sandroabashidze <abashidzesandro21@gmail.com>
2024-05-31 22:27:02 +0400

    second commit

---
+++ level 1/homework/lesson 1.py
@@ -0,0 +1,7 @@
+name ="luka"
+surname="abashidze"
+age=10
+height=1.45
+print("me var" + " " + name + " " + surname + " " + "wlovanebit" + " " + str(age) + " " + "simaglit var" + " " +  str(height))  
+
+
