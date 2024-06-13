7e875cc2b03fcea5842f4a5249c3d67395e4c571
sandroabashidze <abashidzesandro21@gmail.com>
2024-06-13 18:11:04 +0400

    second commit

---
+++ level 3/homework/lesson4.py
@@ -0,0 +1,15 @@
+name="luka"
+surname="abashidze"
+country="georgia"
+hobby="programireba"
+
+print(name,surname,country,hobby)
+print(20-9*3+24)
+print(2745-1500/2)
+print(290-90+400/20+45*820)
+print(909-908+20-10)
+print(200000-100000+50*1000)
+print(10-4+200)
+print(20/20*3-2)
+print(100+100/200+45-15)
+print(35+100-20+8*2)
