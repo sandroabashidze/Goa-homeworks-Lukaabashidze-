28a9ddfa34b0ed5506f63dbcd8c08566c0f671fd
sandroabashidze <abashidzesandro21@gmail.com>
2024-06-28 14:55:27 +0400

    second commit

---
+++ level 7/homework/level 7.py
@@ -0,0 +1,56 @@
+
+
+
+a=int(input("enter any number: "))
+b=int(input("enter any number: "))
+print(a + b)
+print(a - b)
+print(a / b)
+print(a // b)
+print(a * b)
+print(a ** b)
+
+c=int(input("enter any number: "))
+d=int(input("enter any number: "))
+print(c + d)
+print(c - d)
+print(c / d)
+print(c // d)
+print(c * d)
+print(c ** d)
+
+
+mum_age=int(38)
+print(mum_age + 20)
+
+dad_age=int(35)
+print(dad_age + 20)
+
+brother_age=int(13)
+print(brother_age + 20)
+
+grandma_age=int(64)
+print(grandma_age + 20)
+
+grandpa_age=int(69)
+print(grandpa_age + 20)
+
+
+my_name="luka"
+my_lastname="abashidze"
+my_age=str(10)
+my_favcolor="blue"
+live_in="georgia"
+my_favfood="everything sweet"
+fav_car="Bugatti"
+fav_sport="Football"
+print('my name is'+" "+ my_name+ " " + my_lastname + " " + "i am" + " "+ my_age+"years old and i live in" + " " + live_in +" "+ "my favourite food is"+" " +my_favfood+"my favourite sport is"+ " "+fav_sport+"my favourite color and car are"+" "+fav_car+" " +my_favcolor)
+
+
+name=input("enter name: ")
+print("dear"+" "+name+ " "+"happy new year!i hope you all are good see you soon!!" )
+
+tool="axe"
+price=int(50)
+how_many=int(2)
+print(price * how_many)
